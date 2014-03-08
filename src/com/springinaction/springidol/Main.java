e2c3416002972f99b9661a8730cea8162ff0601f
Costas Polychronopoulos <costas_poly@hotmail.com>
2014-03-08 21:03:40 +0200

    fixes: #1

--- src/com/springinaction/springidol/Main.java
+++ src/com/springinaction/springidol/Main.java
@@ -8,25 +8,19 @@ import com.springinaction.springidol.exceptions.PerformanceException;
 import com.springinaction.springidol.interfaces.Performer;
 
 @SuppressWarnings("unused")
-public class Main
-{
+public class Main {
 
-    private static ApplicationContext ctx;
+	private static ApplicationContext ctx;
 
-	public static void main (String[] args) throws PerformanceException
-    {
-        ctx = new ClassPathXmlApplicationContext(new String[]
-        		{	"com/springinaction/springidol/concrete/performers/performers.xml",
-        			"com/springinaction/springidol/concrete/instruments/instruments.xml",
-        			"com/springinaction/springidol/concrete/poems/poems.xml",
-        		});
-        		
-        Performer performer = (Performer) ctx.getBean("carl");
-        performer.perform();
-        
-//        Instrumentalist instrumentalist = (Instrumentalist) ctx.getBean("kenny");
-//        instrumentalist.perform();
+	public static void main(String[] args) throws PerformanceException {
+		ctx = new ClassPathXmlApplicationContext(
+				new String[] {
+						"com/springinaction/springidol/concrete/performers/performers.xml",
+						"com/springinaction/springidol/concrete/instruments/instruments.xml",
+						"com/springinaction/springidol/concrete/poems/poems.xml", });
 
-    }
+		Performer performer = (Performer) ctx.getBean("carl");
+		performer.perform();
+	}
 
 }
